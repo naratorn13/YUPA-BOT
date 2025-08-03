25ea7c2302655293c1f537e7e9ae7927fe6ed7da
naratorn <dotimepod@gmail.com>
2025-08-03 15:24:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -97,6 +97,9 @@ def webhook():
         return jsonify({"status": "invalid action", "data": data})
 
 # === RUN APP ===
+from waitress import serve
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    serve(app, host="0.0.0.0", port=port)
