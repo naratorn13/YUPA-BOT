ace094a7465b7669cbbc94d13f520162c332af5d
naratorn <dotimepod@gmail.com>
2025-08-04 10:46:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,7 @@ import os
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 API_PASSPHRASE = os.getenv("API_PASSPHRASE")
-
-
-print("DEBUG API_KEY:", API_KEY)
-print("DEBUG API_SECRET:", API_SECRET)
-print("DEBUG API_PASSPHRASE:", API_PASSPHRASE)
 BASE_URL = 'https://www.okx.com'
-
 
 # === SIGNATURE GENERATOR ===
 def generate_signature(timestamp, method, request_path, body=''):
