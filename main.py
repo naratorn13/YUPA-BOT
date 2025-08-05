8ce8e630c3101559e6e110d53f2ac33124a5e985
naratorn <dotimepod@gmail.com>
2025-08-05 11:35:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -82,4 +82,5 @@ def webhook():
         return jsonify({"status": "error", "msg": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # แก้ไขตรงนี้สำหรับ production จริง
+    app.run(host='0.0.0.0', port=5000)
